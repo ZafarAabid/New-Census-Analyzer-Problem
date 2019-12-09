7721b1ecd11cae4e393c631663984b7d41e191ce
zafaraabid <zafaraabid3295@gmail.com>
2019-12-09 12:18:07 +0530

    Add.written a testCase to check generic to work for StateCode and CensusData

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -9,6 +9,7 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     /*/home/user/workspace/newCode/src/main/resources/StateCensusData.csv*/
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String STATE_CODE_CSV = "/home/user/workspace/newCode/src/test/resources/IndiaStateCode.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -32,4 +33,16 @@ public class CensusAnalyserTest {
 
 
     }
+
+    @Test
+    public void givenindianStateCsv__shouldReturnExactCount() {
+        try {
+            CensusAnalyser censusAnalyser= new CensusAnalyser();
+            int  numberOfEntries = censusAnalyser.loadIndiaStateCode(STATE_CODE_CSV);
+            Assert.assertEquals(37,numberOfEntries);
+
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+       }
 }
